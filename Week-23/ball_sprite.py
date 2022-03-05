07984d90e4a439edc33c1bcd32dddfcfc6773e19
Weizhe Li <weizheli@gmail.com>
2022-03-05 16:30:33 -0500

    Update ball_sprite.py

--- Week-23/ball_sprite.py
+++ Week-23/ball_sprite.py
@@ -2,7 +2,6 @@ import pygame, sys
 pygame.init()
 screen = pygame.display.set_mode([640,480])
 screen.fill([255, 255, 255])
-my_ball = pygame.image.load('wackyball.bmp')
 
 class myBall(pygame.sprite.Sprite):
     def __init__(self, image, location):
@@ -10,6 +9,7 @@ class myBall(pygame.sprite.Sprite):
         self.ballImage = pygame.image.load(image)
         self.rect = self.ballImage.get_rect()
         self.rect.left, self.rect.top = location
+        self.speed = speed
         
     def move(self):
         self.rect = self.rect.move(self.speed)
@@ -17,3 +17,21 @@ class myBall(pygame.sprite.Sprite):
             self.speed[0] = -self.speed[0]
         if self.rect.top < 0 or self.rect.bottom > height:
             self.speed[1] = -self.speed[1]
+        
+img_file = "wackyball.bmp"
+
+
+ball = Ball(img_file, [200, 200], [3, 3])
+# Adds each ball to the list as it’s created
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.time.delay(20)
+    # Redraws the screen
+    screen.fill([255, 255, 255])
+    ball.move()
+    screen.blit(ball.image, ball.rect)
+    pygame.display.flip()
+pygame.quit()
